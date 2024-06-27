7f607e10cc6dcef99ba098f6ffbfea98451c223e
Mikel Jimenez <majc1907@gmail.com>
2024-06-27 18:28:14 +0200

    trying-images-again

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -9,13 +9,13 @@ function Footer(){
     </span>
     <ul class="flex flex-wrap items-center mt-3 text-sm font-medium text-gray-500 dark:text-gray-400 sm:mt-0">
         <li>
-            <a href= "https://www.linkedin.com/in/miguel-angel-jimenez-calcedo-web-dev/" target="_blank" class="hover:underline me-4 md:me-6"> <img src="./public/images/icons8-linkedin-logo-30.png" alt="linkedin" /> </a>
+            <a href= "https://www.linkedin.com/in/miguel-angel-jimenez-calcedo-web-dev/" target="_blank" class="hover:underline me-4 md:me-6"> <img src="/images/icons8-linkedin-logo-30.png" alt="linkedin" /> </a>
         </li>
         <li>
-            <a href="https://github.com/Saymar567" target="_blank" class="hover:underline me-4 md:me-6"><img src="./public/images/icons8-github-30.png" alt="github" /></a>
+            <a href="https://github.com/Saymar567" target="_blank" class="hover:underline me-4 md:me-6"><img src="/images/icons8-github-30.png" alt="github" /></a>
         </li>
         <li>
-            <a href="mailto:majc1907@gmail.com" class="hover:underline me-4 md:me-6"><img src="./public/images/icons8-mail-30.png" alt="mail" /></a>
+            <a href="mailto:majc1907@gmail.com" class="hover:underline me-4 md:me-6"><img src="/images/icons8-mail-30.png" alt="mail" /></a>
         </li>
        
     </ul>
