de150fe051b490adbd3b12cd4571a99fe7d5a281
Mikel Jimenez <majc1907@gmail.com>
2025-09-16 21:13:55 +0200

    change

--- src/Components/VisitCounter.jsx
+++ src/Components/VisitCounter.jsx
@@ -1,36 +1,36 @@
 import { useState, useEffect } from "react";
 import supabase from "../../supabaseClient";
 
-const VisitCounter = () => {
-    const [visits, setVisits] = useState(0);
+// const VisitCounter = () => {
+//     const [visits, setVisits] = useState(0);
 
-    useEffect(() => {
-        const visitCount = async () => {
-            const { data, error } = await supabase
-                .from('visits')
-                .select('count')
-                .single()
+//     useEffect(() => {
+//         const visitCount = async () => {
+//             const { data, error } = await supabase
+//                 .from('visits')
+//                 .select('count')
+//                 .single()
 
-            if (error) { console.error("Error getting data", error); return }
-            if (data) {
-                const updatedCount = data.count + 1;
-                setVisits(updatedCount);
+//             if (error) { console.error("Error getting data", error); return }
+//             if (data) {
+//                 const updatedCount = data.count + 1;
+//                 setVisits(updatedCount);
 
-                const { error: updateError } = await supabase
-                    .from('visits')
-                    .update({ count: updatedCount })
-                    .eq('id', 1);
-            }
-        }
-        visitCount();
+//                 const { error: updateError } = await supabase
+//                     .from('visits')
+//                     .update({ count: updatedCount })
+//                     .eq('id', 1);
+//             }
+//         }
+//         visitCount();
 
-    }, [])
+//     }, [])
 
-    return (
-        <div>
-            <p>Number of visits: {visits}</p>
-        </div>
-    )
-}
+//     return (
+//         <div>
+//             <p>Number of visits: {visits}</p>
+//         </div>
+//     )
+// }
 
-export default VisitCounter
+// export default VisitCounter
